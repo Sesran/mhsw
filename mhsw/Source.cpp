47ee2b18ff364b6e3d4558bc7495131bfb47337e
Sesran <e.wisc@yahoo.fr>
2018-03-02 17:46:34 +0100

    fixed problem with release and debug libraries being mixed, added button class

--- mhsw/Source.cpp
+++ mhsw/Source.cpp
@@ -1,22 +1,37 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "button.h"
+
+using namespace sf;
+
+//Window size constants
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	//Create menu window
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Monster Hunter Small World");
 
+	//std::cout << font.getLineSpacing(32) << std::endl;
+
+	//Create play button
+	Button playButton(300,50,"Play");
+	playButton.setPosition(WINDOW_WIDTH / 2 - 150, WINDOW_HEIGHT / 2 - 25);
+
+	//Main loop
 	while (window.isOpen())
 	{
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 				window.close();
 		}
 
 		window.clear();
-		window.draw(shape);
+		//Draw objects
+		playButton.draw(&window);
 		window.display();
 	}
 
